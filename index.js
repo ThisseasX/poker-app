1fe8c51e98ad193478fae864dffd9dbc7956192c
Thisseas X <thisseasx@yahoo.com>
2019-08-15 20:31:06 +0300

    Insanity with quality

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const { parseHand } = require('./utils');
-const {
-  isRoyalFlush,
-  isStraightFlush,
-  isFourOfAKind,
-  isFullHouse,
-  isFlush,
-  isStraight,
-  isThreeOfAKind,
-  isTwoPair,
-  isPair,
-} = require('./hand-rankings');
+const rankings = require('./hand-rankings');
+
+const transformRankingName = name =>
+  name
+    .replace(/is/, '')
+    .replace(/(?<=\w)([A-Z])/g, `_$1`)
+    .toUpperCase();
+
+const hand = [
+  { id: 1, rank: 2, suit: 1 },
+  { id: 14, rank: 2, suit: 2 },
+  { id: 15, rank: 3, suit: 2 },
+  { id: 29, rank: 4, suit: 3 },
+  { id: 43, rank: 5, suit: 4 },
+];
 
 const rate = hand => {
+  let result = { name: 'HIGH_CARD', quality: 0 };
+
   const handStatistics = parseHand(hand);
 
-  switch (true) {
-    case isRoyalFlush(handStatistics):
-      return 'ROYAL_FLUSH';
-    case isStraightFlush(handStatistics):
-      return 'STRAIGHT_FLUSH';
-    case isFourOfAKind(handStatistics):
-      return 'FOUR_OF_A_KIND';
-    case isFullHouse(handStatistics):
-      return 'FULL_HOUSE';
-    case isFlush(handStatistics):
-      return 'FLUSH';
-    case isStraight(handStatistics):
-      return 'STRAIGHT';
-    case isThreeOfAKind(handStatistics):
-      return 'THREE_OF_A_KIND';
-    case isTwoPair(handStatistics):
-      return 'TWO_PAIR';
-    case isPair(handStatistics):
-      return 'PAIR';
-    default:
-      return 'HIGH_CARD';
-  }
+  const transformedRankings = Object.entries(rankings).map(
+    ([name, ranking]) => [transformRankingName(name), ranking],
+  );
+
+  transformedRankings.some(
+    ([name, ranking], i, arr) =>
+      ranking(handStatistics) &&
+      (result = {
+        name,
+        quality: arr.length - i,
+      }),
+  );
+
+  return result;
 };
+
+console.log(rate(hand));
 
 module.exports = { rate };
