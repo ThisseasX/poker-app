ba7869949a7b24f61ee5643b56f4304c9258c019
Thisseas X <thisseasx@yahoo.com>
2019-08-15 20:31:35 +0300

    Insanity without quality

--- index.js
+++ index.js
@@ -7,16 +7,8 @@ const transformRankingName = name =>
     .replace(/(?<=\w)([A-Z])/g, `_$1`)
     .toUpperCase();
 
-const hand = [
-  { id: 1, rank: 2, suit: 1 },
-  { id: 14, rank: 2, suit: 2 },
-  { id: 15, rank: 3, suit: 2 },
-  { id: 29, rank: 4, suit: 3 },
-  { id: 43, rank: 5, suit: 4 },
-];
-
 const rate = hand => {
-  let result = { name: 'HIGH_CARD', quality: 0 };
+  let result = 'HIGH_CARD';
 
   const handStatistics = parseHand(hand);
 
@@ -25,17 +17,10 @@ const rate = hand => {
   );
 
   transformedRankings.some(
-    ([name, ranking], i, arr) =>
-      ranking(handStatistics) &&
-      (result = {
-        name,
-        quality: arr.length - i,
-      }),
+    ([name, ranking]) => ranking(handStatistics) && (result = name),
   );
 
   return result;
 };
-
-console.log(rate(hand));
 
 module.exports = { rate };
